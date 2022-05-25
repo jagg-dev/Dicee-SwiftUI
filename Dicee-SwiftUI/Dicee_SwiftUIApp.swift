96e0340391e6e43271530145ce6242d8237f7251
Jorge Gonzalez <jagg.dev.27@gmail.com>
2022-05-25 14:58:49 -0500

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Jorge Gonzalez on 25/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
